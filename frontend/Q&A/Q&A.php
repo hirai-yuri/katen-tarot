ca388e5a72ce75bf07225f73d3b39ebc218ef253
kouka-nayuta <tengyuanxiuxiang@gmail.com>
2024-10-03 11:21:55 +0900

    変更と追加PHP

---
+++ frontend/Q&A/Q&A.php
@@ -0,0 +1,20 @@
+<?php
+mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+
+$name = $_POST['name'];
+$birthday = $_POST['birthday'];
+$Email = $_POST['Email'];
+$THOTH = $_POST['THOTH'];
+$address = $_POST['address'];
+$gmail = "From: yoshinori52671@gmail.com";
+
+if(mb_send_mail($name, $birthday, $Email,$THOTH ,$address,$gmail))
+{
+    echo "メール送信成功です";
+}
+else
+{
+    echo "メール送信失敗です";
+}
+?>
