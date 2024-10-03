6d639f01350915201c7dc34434be2a330d77e1ec
kouka-nayuta <tengyuanxiuxiang@gmail.com>
2024-10-03 13:25:34 +0900

    一応修正版

--- frontend/Q&A/Q&A.php
+++ frontend/Q&A/Q&A.php
@@ -9,12 +9,10 @@ $THOTH = $_POST['THOTH'];
 $address = $_POST['address'];
 $gmail = "From: yoshinori52671@gmail.com";
 
-if(mb_send_mail($name, $birthday, $Email,$THOTH ,$address,$gmail))
-{
-    echo "メール送信成功です";
+if (mb_send_mail($name, $birthday, $Email, $THOTH, $address, $gmail)) {
+  echo "メール送信成功です";
+} elseif (empty($_POST['check_data'])) {
+  echo "未記入があります。";
+} else {
+  echo "メール送信失敗です";
 }
-else
-{
-    echo "メール送信失敗です";
-}
-?>
