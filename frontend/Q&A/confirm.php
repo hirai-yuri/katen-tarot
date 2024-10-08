ea6228875ea970442fdea15c3d286b6441186fe0
kouka-nayuta <tengyuanxiuxiang@gmail.com>
2024-10-08 11:54:09 +0900

    メールフォーム

--- frontend/Q&A/confirm.php
+++ frontend/Q&A/confirm.php
@@ -1,15 +1,14 @@
 <?php
 //直リンクされた場合index.phpにリダイレクト
 if($_SERVER["REQUEST_METHOD"] != "POST"){
-	header("Location: Q&A.php");
+	header("Location: Q&A.html");
 	exit();
 }
 
 //各項目を内容を取得
 $name = $_POST['name'];
-$birthday = $_POST['birthday'];
 $Email = $_POST['Email'];
-$THOTH = $_POST['THOTH'];
+$sex = $_POST['sex'];
 $address = $_POST['address'];
 //チェックボックスの内容をスラッシュ区切りで取り出す
 $reason = implode('/', $_POST['reason']);
@@ -20,10 +19,10 @@ $reason = implode('/', $_POST['reason']);
 <meta charset="utf-8">
 <title>確認画面</title>
 <style type="text/css">
-	body {
+	/* body {
 		background-color: #f9fff2;
 	}
-	.input-area {
+	.tarot-area {
 		margin-bottom: 20px;
 	}
 	p {
@@ -47,50 +46,44 @@ $reason = implode('/', $_POST['reason']);
 		background-color: #9ec34b;
 		border-color: #cbe585;
 		color: #FFF;
-	}
+	} */
 </style>
 </head>
 <body>
- 	<form action="complete.php" method="post">
+ 	<form action="Q&A.php" method="post">
  		<h2>お問い合わせ内容確認</h2>
-	 	<div class="input-area">
+	 	<div class="tarot-area">
 		 	<p>名前</p>
 		 	<?php echo htmlspecialchars($name,ENT_QUOTES,'UTF-8');?>
 		</div>
 
-		<div class="input-area">
-		 	<p>メール</p>
-		 	<?php echo htmlspecialchars($email,ENT_QUOTES,'UTF-8');?>
+		<div class="tarot-area">
+		 	<p>メールアドレス</p>
+		 	<?php echo htmlspecialchars($Email,ENT_QUOTES,'UTF-8');?>
 	 	</div>
 
-	 	<div class="input-area">
+	 	<div class="tarot-area">
 		 	<p>性別</p>
 		 	<?php echo $sex;?>
 	 	</div>
 
-	 	<div class="input-area">
-		 	<p>お住まいの地域</p>
-		 	<?php echo $pref;?>
-	 	</div>
-
-	 	<div class="input-area">
+	 	<div class="tarot-area">
 		 	<p>お問い合わせ理由</p>
 		 	<?php echo $reason;?>
 	 	</div>
 
-		<div class="input-area">
+		<div class="tarot-area">
 	 		<p>お問い合わせ内容</p>
-	 		<?php echo nl2br(htmlspecialchars($contact_body,ENT_QUOTES,'UTF-8'));?>
+	 		<?php echo nl2br(htmlspecialchars($address,ENT_QUOTES,'UTF-8'));?>
 		</div>
-	 	<div class="input-area">
+	 	<div class="tarot-area">
 	 		<input type='button' onclick='history.back()' value='戻る' class="btn-border">
 	 		<input type="submit" name="submit" value="送信" class="btn-border">
 	 		<input type="hidden" name="name" value="<?php echo $name;?>">
-	 		<input type="hidden" name="email" value="<?php echo $email;?>">
+	 		<input type="hidden" name="Email" value="<?php echo $Email;?>">
 	 		<input type="hidden" name="sex" value="<?php echo $sex;?>">
-	 		<input type="hidden" name="pref" value="<?php echo $pref;?>">
 	 		<input type="hidden" name="reason" value="<?php echo $reason;?>">
-	 		<input type="hidden" name="contact_body" value="<?php echo $contact_body;?>">
+	 		<input type="hidden" name="address" value="<?php echo $address;?>">
 	 	</div>
 	</form>
 </body>
