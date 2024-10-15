9f213d101cc70aee00f7bd79997a499d48dde788
kouka-nayuta <tengyuanxiuxiang@gmail.com>
2024-10-15 12:08:10 +0900

    組み換え

--- frontend/Q&A/mordar.js
+++ frontend/Q&A/mordar.js
@@ -1,25 +1,34 @@
 'use strict';
-$(document).ready(function() {				
-  const $body = $('body');				
-  const $modal = $('#modal');				
-  const $modalOpenButton = $('.js-modal-open');				
-  const $modalCloseButton = $('.js-modal-close');				
+$(function() {
+  $('#exampleModal').on('show.bs.modal', function () {
+    var title = $('#formTitle').val()
+    var body = $('#formBody').val()
+    var modal = $(this)
+    modal.find('#modalTitle').text(title)
+    modal.find('#modalBody').text(body)
+  })
+})
+// $(document).ready(function() {				
+//   const $body = $('body');				
+//   const $modal = $('#modal');				
+//   const $modalOpenButton = $('.js-modal-open');				
+//   const $modalCloseButton = $('.js-modal-close');				
           
-  const openModal = () => {				
-  $modal.fadeIn();				
-  $body.css('overflow', 'hidden');				
-  };				
+//   const openModal = () => {				
+//   $modal.fadeIn();				
+//   $body.css('overflow', 'hidden');				
+//   };				
           
-  const closeModal = () => {				
-  $modal.fadeOut();				
-  $body.css('overflow', 'auto');				
-  };				
+//   const closeModal = () => {				
+//   $modal.fadeOut();				
+//   $body.css('overflow', 'auto');				
+//   };				
           
-  const onClickOutside = (event) => {				
-  if ($modal.is(event.target)) closeModal();				
-  }				
+//   const onClickOutside = (event) => {				
+//   if ($modal.is(event.target)) closeModal();				
+//   }				
           
-  $modalOpenButton.on('click', openModal);				
-  $modalCloseButton.on('click', closeModal);				
-  $modal.on('click', onClickOutside);				
-  });	
+//   $modalOpenButton.on('click', openModal);				
+//   $modalCloseButton.on('click', closeModal);				
+//   $modal.on('click', onClickOutside);				
+//   });				
