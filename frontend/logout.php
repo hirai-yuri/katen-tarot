afeef4b5ac31fba2c626a05aab35ddaaafd340f2
hirai-yuri <163645242+hirai-yuri@users.noreply.github.com>
2024-10-17 12:06:06 +0900

    ログアウトするとセッションのユーザー名が消えてしまう現象を修正

--- frontend/logout.php
+++ frontend/logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-session_destroy(); // セッションを破棄
-header("Location: ./index.php"); // index.phpにリダイレクト
+unset($_SESSION['user_id']); // 特定のセッション変数を削除
+header("Location: ./index.php");
 exit();
