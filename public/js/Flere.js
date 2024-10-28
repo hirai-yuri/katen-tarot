4b7bd36891e8e202f7f7ed792e7cf396e4e3c4a7
kouka-nayuta <tengyuanxiuxiang@gmail.com>
2024-10-28 09:47:56 +0900

    一時的

--- public/js/Flere.js
+++ public/js/Flere.js
@@ -5,10 +5,11 @@ document.getElementById('Flere210').onclick=function(event){
   ele.style.display = 'block';
   document.form.submit();
 }
-$(window).on('load',function(){
-  $("#loading").delay(12000).fadeOut('slow');
+
+// $(window).on('load',function(){
+//   $("#loading").delay(12000).fadeOut('slow');
   //ローディング画面を1.2秒（1200ms）待機してからフェードアウト
-});
+// });
 var bar = new ProgressBar.Line(loading_text, {//id名を指定
   strokeWidth: 0,//進捗ゲージの太さ
   duration: 5100,//時間指定(1000＝5秒)
@@ -30,8 +31,8 @@ var bar = new ProgressBar.Line(loading_text, {//id名を指定
     bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
   }
 });
-bar.animate(1.0, function () {
+// bar.animate(1.0, function () {
   //バーを描画する割合を指定します 1.0 なら100%まで描画
-  $("#loading_text").delay(0).fadeOut('fast');
+  // $("#loading_text").delay(0).fadeOut('fast');
   //アニメーションが終わったら#loading_textをフェードアウト
-});
+// });
